3933356d5f6f785f04f0a9652b7fb015b80f9e21
dougbtv <dosmith@redhat.com>
2024-05-15 08:41:54 -0400

    removed face editor

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -20,7 +20,6 @@ PIP_PACKAGES=(
 EXTENSIONS=(
     "https://github.com/Mikubill/sd-webui-controlnet"
     "https://github.com/adieyal/sd-dynamic-prompts"
-    "https://github.com/ototadana/sd-face-editor"
     "https://github.com/AlUlkesh/stable-diffusion-webui-images-browser"
     "https://github.com/hako-mikan/sd-webui-regional-prompter"
     "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
